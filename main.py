81317e066fb644d850f50fe19c39e19dc92374e8
Rohit Poduval <rohit.poduval2@gmail.com>
2024-12-12 19:29:56 -0600

    Format price printing

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cv2
 import pandas as pd
 import numpy as np
 import statistics
-from collections import Counter
 
 from helpers import receipt_to_df
 
@@ -13,23 +12,27 @@ message = "Upload your receipts. Ensure that the receipt contains the date and i
 receipts = st.file_uploader(message, ["png", "jpg"], accept_multiple_files=True)
 if receipts is not None:
     for receipt in receipts:
+        # Convert file upload into opencv readable format
         file_bytes = np.frombuffer(receipt.read(), np.uint8)
         receipt = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+
+        # Extract relevant information from receipt
         df = receipt_to_df(receipt)
         df.to_csv('receipt_data.csv', mode='a', index=False, header=False)
 
+
 # Data Processing
-mm_sum = {}
-name_occur = {}
-with open("receipt_data.csv", "r") as f:
+mm_sum = {}      # the total amount spent for each month
+item_occur = {}  # item name and the number of times it was purchased
+with open(file="receipt_data.csv", mode="r", encoding="UTF-8") as f:
     for line in f:
-        date, name, price = line.split(",")
+        date, item, price = line.split(",")
         price = float(price)
 
-        if name not in name_occur:
-            name_occur[name] = 1
+        if item not in item_occur:
+            item_occur[item] = 1
         else:
-            name_occur[name] += 1
+            item_occur[item] += 1
 
         mm, dd, yy = date.split("/")
         mm = int(mm)
@@ -38,7 +41,7 @@ with open("receipt_data.csv", "r") as f:
         else:
             mm_sum[mm] += price
 
-name_occur = sorted(name_occur.items(), key=lambda x:x[1])
+item_occur = sorted(item_occur.items(), key=lambda x:x[1])
 
 mm_month = {
     1: "January",
@@ -57,14 +60,14 @@ mm_month = {
 
 
 # At a Glance
-max_month = mm_month[max(mm_sum, key=mm_sum.get)]
-st.write(f"Max Spending Month: {max_month}")
-st.write(f"Mean Monthly Spending: ${statistics.mean(mm_sum.values())}")
+max_spending_month = mm_month[max(mm_sum, key=mm_sum.get)]
+st.write(f"Max Spending Month: {max_spending_month}")
+st.write(f"Mean Monthly Spending: ${statistics.mean(mm_sum.values()):.2f}")
 st.write("Top 3 Most Purchased Items:")
 top_3_md = f"""
-1. {name_occur[-1][0]} {"were" if name_occur[-1][0].endswith("S") else "was"} bought {name_occur[-1][1]} times
-2. {name_occur[-2][0]} {"were" if name_occur[-2][0].endswith("S") else "was"} bought {name_occur[-2][1]} times
-3. {name_occur[-3][0]} {"were" if name_occur[-3][0].endswith("S") else "was"} bought {name_occur[-3][1]} times
+1. {item_occur[-1][0]} {"were" if item_occur[-1][0].endswith("S") else "was"} bought {item_occur[-1][1]} times
+2. {item_occur[-2][0]} {"were" if item_occur[-2][0].endswith("S") else "was"} bought {item_occur[-2][1]} times
+3. {item_occur[-3][0]} {"were" if item_occur[-3][0].endswith("S") else "was"} bought {item_occur[-3][1]} times
 """
 st.markdown(top_3_md)
 
